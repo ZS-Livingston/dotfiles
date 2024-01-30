60febb63ae8ce338baada01b4fe4202a75419fcc
albert <albert.radvan@zslivingston.cz>
2024-01-30 14:15:18 +0100

    fix: change install_script pwd

--- install_script.sh
+++ install_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Determine the directory where this script is located
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Define the home directory
 HOME_DIR="$HOME"
-
-# Define the directory where your dotfiles repository is cloned
-REPO_DIR="$HOME/dotfiles"
 
 # Copy the .vimrc file from the repository to the home directory
 cp "$REPO_DIR/.vimrc" "$HOME_DIR"
